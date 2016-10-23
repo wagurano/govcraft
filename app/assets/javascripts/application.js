0fe1dbeb721b3e5205fc576a28d59c67c79279ca
rest515 <rest515@gmail.com>
2016-10-23 20:17:39 +0900

    masonry 라이브러리 추가

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,5 +3,14 @@
 //= require bootstrap
 //= require unobtrusive_flash
 //= require unobtrusive_flash_bootstrap
+//= require imagesloaded.pkgd
+//= require masonry.pkgd
 
 UnobtrusiveFlash.flashOptions['timeout'] = 300000;
+
+$(document).imagesLoaded( { }, function() {
+    //masonry
+    var options = {}
+    $('.masonry-container').masonry();
+  }
+);
