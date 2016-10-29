00a9f18fc650a1fa80a038871e6f58db6454a5ff
dali <dalikim@parti.xyz>
2016-10-29 09:27:29 +0900

    좋아요에 중복 체크 버그 수정

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,5 +2,5 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :likable, polymorphic: true, counter_cache: true
 
-  validates :user, uniqueness: { scope: :likable }
+  validates :user, uniqueness: { scope: [:likable_id, :likable_type] }
 end
