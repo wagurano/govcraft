1440aaf8353d69ea347cd75c9355806fae9dbb0a
rest515 <rest515@gmail.com>
2016-11-04 20:22:14 +0900

    공유버튼을 네이티브로 변경합니다

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,9 +35,58 @@ $(document).imagesLoaded( { }, function() {
 
 $(function(){
   $('.share-box').jsSocials({
-    showCount: true,
     showLabel: false,
-    shares: ["email", "twitter", "facebook"]
+    showCount: false,
+
+    shares: [{
+        renderer: function() {
+            var $result = $("<div>");
+
+            var script = document.createElement("script");
+            script.text = "(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = \"//connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.3\"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk'));";
+            $result.append(script);
+
+            $("<div>").addClass("fb-share-button")
+                .attr("data-layout", "button_count")
+                .appendTo($result);
+
+            return $result;
+        }
+    }, {
+        renderer: function() {
+            var $result = $("<div>");
+
+            var script = document.createElement("script");
+            script.text = "window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return t;js=d.createElement(s);js.id=id;js.src=\"https://platform.twitter.com/widgets.js\";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,\"script\",\"twitter-wjs\"));";
+            $result.append(script);
+
+            $("<a>").addClass("twitter-share-button")
+                .text("Tweet")
+                .attr("href", "https://twitter.com/share")
+                .appendTo($result);
+
+            return $result;
+        }
+    }, {
+        renderer: function() {
+            var $result = $("<div>");
+
+            var script = document.createElement("script");
+            script.src = "//assets.pinterest.com/js/pinit.js";
+            $result.append(script);
+
+            $("<a>").append($("<img>").attr("//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_red_20.png"))
+                    .attr({
+                        href: "//www.pinterest.com/pin/create/button/?url=http%3A%2F%2Fjs-socials.com%2Fdemos%2F&media=%26quot%3Bhttp%3A%2F%2Fgdurl.com%2Fa653%26quot%3B&description=Next%20stop%3A%20Pinterest",
+                        "data-pin-do": "buttonPin",
+                        "data-pin-config": "beside",
+                        "data-pin-color":"red"
+                    })
+                    .appendTo($result);
+
+            return $result;
+        }
+    }]
   });
 
   $('.post-block__body iframe').addClass('embed-responsive-item');
