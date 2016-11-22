6682e0710f399b56d9855eda4967f75df180d3ea
rest515 <rest515@gmail.com>
2016-11-22 10:26:51 +0900

    아카이브 JSON을 CORS에서 허용한다

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,6 @@
 Rails.application.config.middleware.insert_before 0, "Rack::Cors" do
   allow do
-    origins 'localhost:4200'
-    resource '*',
-      headers: :any,
-      methods: %i(get post put patch delete options head)
+    origins '*'
+    resource '/archives/*.json', headers: :any, methods: :get
   end
 end
