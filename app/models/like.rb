98edab41d1d3fe5890935de7bc808e27262368d2
rest515 <rest515@gmail.com>
2016-11-27 20:19:15 +0900

    공감 버튼을 ajax로 바꾸고 디폴트 상태를 설정합니다

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,8 @@ class Like < ApplicationRecord
   belongs_to :likable, polymorphic: true, counter_cache: true
 
   validates :user, uniqueness: { scope: [:likable_id, :likable_type] }
+
+  def self.liked?(likable, user)
+    Like.exists?(likable: likable, user: user)
+  end
 end
