5ccbdaf7d0b9245d7db96cd62c0abbafc2aea0b0
qus <qus1225@gmail.com>
2016-12-08 12:43:35 +0900

    비로그인 사용자도 서명참여자수 더보기를 할 수 있습니다

--- app/controllers/signs_controller.rb
+++ app/controllers/signs_controller.rb
@@ -1,5 +1,5 @@
 class SignsController < ApplicationController
-  before_action :authenticate_user!, except: :create
+  before_action :authenticate_user!, except: [:create, :index]
   load_and_authorize_resource
 
   def index
