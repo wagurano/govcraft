0a7f098a31f6f7cff846543461308672b955727a
dali <dalikim@parti.xyz>
2016-12-20 13:38:38 +0900

    캠페인별로 게시판, 서명, 투표, 위키의 이름을 지정하지 않으면 기본값이 보입니다.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -9,4 +9,10 @@ class Campaign < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   scope :recent, -> { order('id DESC') }
+
+  def component_title(modle_name)
+    model = modle_name.classify.safe_constantize
+    return if model.blank?
+    send(:"#{modle_name}_title").blank? ? model.model_name.human : send(:"#{modle_name}_title")
+  end
 end
