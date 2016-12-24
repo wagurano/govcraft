277e1aefd62c86a4016412cd64a98a590adb79e6
dali <dalikim@parti.xyz>
2016-12-24 12:38:37 +0900

    thumbs 데이터베이스 스키마 오류를 바로 잡습니다

--- db/migrate/20161223073928_create_thumbs.rb
+++ db/migrate/20161223073928_create_thumbs.rb
@@ -1,6 +1,6 @@
 class CreateThumbs < ActiveRecord::Migration[5.0]
   def change
-    create_table :thumbs do |t|
+    create_table :thumbs, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC" do |t|
       t.references :user, index: true
       t.references :thumbable, polymorphic: true, index: true
       t.string :direction, null: false
