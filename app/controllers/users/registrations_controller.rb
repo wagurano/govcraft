7c478a297051da161eb8f61aae85f3a0d15b1fe6
dali <dalikim@parti.xyz>
2017-01-03 12:20:04 +0900

    프로필에서 우주당 소식 메일링 여부를 설정합니다

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -13,7 +13,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:remember_me, :nickname, :description, :image, :email)
+    params.require(:user).permit(:remember_me, :nickname, :description, :image, :email, :enable_mailing)
   end
 
   def after_inactive_sign_up_path_for(resource)
