7ecfdd69f08ab067da78bc891d08f05c47c2d6d8
dali <dalikim@parti.xyz>
2017-02-13 18:24:09 +0900

    archive_categories 테이블 생성 옵션을 ROW_FORMAT=DYNAMIC로 변경합니다

--- db/migrate/20170213054009_create_archive_categories.rb
+++ db/migrate/20170213054009_create_archive_categories.rb
@@ -1,6 +1,6 @@
 class CreateArchiveCategories < ActiveRecord::Migration[5.0]
   def change
-    create_table :archive_categories do |t|
+    create_table :archive_categories, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC"  do |t|
       t.references :archive, index: true, null: false
       t.references :parent, index: true
       t.string :slug, index: true, null: false
