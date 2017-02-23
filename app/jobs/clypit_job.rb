b847d7d6289a88eb4169f058ca709210ebe59f3c
dali <dalikim@parti.xyz>
2017-02-23 13:19:57 +0900

    아카이브 문서에 음성을 clypit에 올릴때 오류를 잡아서 로그한다

--- app/jobs/clypit_job.rb
+++ app/jobs/clypit_job.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'rest-client'
+require 'open-uri'
 
 class ClypitJob
   include Sidekiq::Worker
@@ -12,50 +13,71 @@ class ClypitJob
       return
     end
 
+    @data = nil
     if document.content.file.respond_to?(:url)
-      data = open document.content.url
+      Dir.mktmpdir do |dir|
+        temp_file = "#{dir}/#{document.content_name}"
+        open(temp_file, 'wb') do |f|
+          f << open(document.content.url).read
+        end
+        @data = open temp_file
+      end
     else
-      data = open document.content.path
+      @data = open document.content.path
     end
 
-
-    auth_response = RestClient.post("https://api.clyp.it/oauth2/token",
-      {
-        grant_type: 'password',
-        username: ENV['CLYPIT_USERNAME'],
-        password: ENV['CLYPIT_PASSWORD']
-      },
-      {Authorization: 'Basic MjkzMTE5Og=='}
-    )
-
-    auth = JSON.parse(auth_response.body)
-    if auth['error'].present?
-      logger.info("Clypit Auth fail : #{auth['error']}")
-      return
-    end
-    access_token = auth['access_token']
-    if access_token.blank?
-      logger.info("Clypit Auth fail : no access_token")
+    if @data.nil?
+      logger.info("Document ##{document.id} : no content data")
       return
     end
 
-    upload_response = RestClient.post("https://upload.clyp.it/upload",
-      {
-        audioFile: data,
-        description: "#{document.title} #{Rails.application.routes.url_helpers.archive_document_url(document)}"
-      },
-      {
-        'X-Client-Type': 'WebAlfa',
-        'Authorization': "Bearer #{access_token}"
-      }
-    )
+    begin
+      auth_response = RestClient.post("https://api.clyp.it/oauth2/token",
+        {
+          grant_type: 'password',
+          username: ENV['CLYPIT_USERNAME'],
+          password: ENV['CLYPIT_PASSWORD']
+        },
+        {Authorization: 'Basic MjkzMTE5Og=='}
+      )
 
-    upload_result = JSON.parse(upload_response.body)
-    if upload_result["Successful"]
-      document.build_clypit(audio_file_id: upload_result["AudioFileId"])
-    else
-      logger.info("Clypit Auth fail : #{upload_result['Title']} - #{upload_result['Description']}")
-      document.build_clypit
+      auth = JSON.parse(auth_response.body)
+      if auth['error'].present?
+        logger.info("Clypit Auth fail : #{auth['error']}")
+        return
+      end
+      access_token = auth['access_token']
+      if access_token.blank?
+        logger.info("Clypit Auth fail : no access_token")
+        return
+      end
+    rescue RestClient::ExceptionWithResponse => e
+      logger.info("Clypit Auth fail : ")
+      logger.info e.inspect
+    end
+
+    begin
+      upload_response = RestClient.post("https://upload.clyp.it/upload",
+        {
+          audioFile: @data,
+          description: "#{document.title} #{Rails.application.routes.url_helpers.archive_document_url(document)}"
+        },
+        {
+          'X-Client-Type': 'WebAlfa',
+          'Authorization': "Bearer #{access_token}"
+        }
+      )
+
+      upload_result = JSON.parse(upload_response.body)
+      if upload_result["Successful"]
+        document.build_clypit(audio_file_id: upload_result["AudioFileId"])
+      else
+        logger.info("Clypit Upload fail : #{upload_result['Title']} - #{upload_result['Description']}")
+        document.build_clypit
+      end
+    rescue RestClient::ExceptionWithResponse => e
+      logger.info("Clypit Upload fail : ")
+      logger.info e.inspect
     end
 
     document.save!
