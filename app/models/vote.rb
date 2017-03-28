048d8f10cddf68de7e68809bf2f5331b7ca37fb6
qus <qus1225@gmail.com>
2017-03-28 11:17:54 +0900

    투표카운트가 표시되고, 여성의제에 대한 opinion에서만 찬반투표가 보입니다

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,7 +4,7 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :votable, polymorphic: true, counter_cache: true
 
-  # counter_culture :votable, :column_name => proc {|model| "#{model.choice}s_count" }
+  counter_culture :votable, :column_name => proc {|model| "#{model.choice}s_count" }
 
   validates :user, uniqueness: { scope: [:votable_id, :votable_type] }, if: "user.present?"
 
