1b54c7ceadc644303d8af7888c46ff84d8c3c003
dali <dalikim@parti.xyz>
2017-04-06 22:49:38 +0900

    이벤트 태그 숫자가 잘못 나오던 것을 바로 잡습니다

---
+++ bin/production_dump/ready_to_download.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ey ssh "sudo -i eybackup -e mysql --download 9:govcraft" -e omurice
---
+++ bin/production_dump/reset_local.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit
+fi
+
+file_name=$1
+branch_namne=`git rev-parse --abbrev-ref HEAD`
+
+. bin/production_dump/parse_yaml.sh
+
+eval $(parse_yaml ./local_env.yml "config_")
+
+if [ ! -z "$config_development_database_password" ]
+then
+  $pass = "-p${config_development_database_password}"
+fi
+
+
+ey scp HOST:/mnt/tmp/$file_name . -e omurice
+mysql -u$config_development_database_username $pass -e "DROP DATABASE govcraft_development_${branch_namne}; CREATE DATABASE govcraft_development_${branch_namne} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
+gunzip -c $file_name | mysql -uroot govcraft_development_$branch_namne
+rm -rf $file_name
