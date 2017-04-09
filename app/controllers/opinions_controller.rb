d50189005ea22c6fc95b940f85d9f36190b58900
rest515 <rest515@gmail.com>
2017-04-10 01:36:08 +0900

    의제의 테마 페이지와 발언자 페이지를 만듭니다

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -2,6 +2,10 @@ class OpinionsController < ApplicationController
   load_and_authorize_resource
   protect_from_forgery except: :vote_widget
 
+  def show
+    redirect_to speaker_path(@opinion.speaker, anchor: "issue_#{@opinion.issue.id}")
+  end
+
   def vote_widget
     @votable = @opinion
     render layout: 'strip_without_footer'
--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -2,11 +2,11 @@ class SpeakersController < ApplicationController
   load_and_authorize_resource
 
   def show
-    if params[:agenda_id].present?
-      @agenda = Agenda.find(params[:agenda_id])
-    elsif params[:tag].present?
+    if params[:tag].present?
       @agendas = Agenda.tagged_with(params[:tag])
       render 'show_by_tag'
+    else
+      @agendas = Agenda.all
     end
   end
 end
