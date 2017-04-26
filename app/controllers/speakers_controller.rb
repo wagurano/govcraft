08c6dbe7a54077faf396e8dbe83d1bc1cc41ad6b
dali <dalikim@parti.xyz>
2017-04-26 17:17:43 +0900

    스피커 상세 페이지의 로딩 속도를 개선합니다

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -3,5 +3,9 @@ class SpeakersController < ApplicationController
 
   def show
     @agendas = Agenda.where(id: Issue.where(id: @speaker.opinions.select(:issue_id)).select(:agenda_id))
+    if params[:agenda_id].present?
+      @agenda = Agenda.find params[:agenda_id]
+      render 'speakers/show_agenda'
+    end
   end
 end
