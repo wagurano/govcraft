03a85ad00e505d04e0654716a1fcf86506291a8a
dali <dalikim@parti.xyz>
2017-09-13 04:31:07 +0900

    기술적인 수정 - 테이블 설정에 오류를 고칩니다

--- db/migrate/20170912182437_create_statement_keys.rb
+++ db/migrate/20170912182437_create_statement_keys.rb
@@ -1,6 +1,6 @@
 class CreateStatementKeys < ActiveRecord::Migration[5.0]
   def change
-    create_table :statement_keys do |t|
+    create_table :statement_keys, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC" do |t|
       t.belongs_to :statement, null: false, index: true
       t.string :key, null: false, index: true
       t.datetime :expired_at, null: false
