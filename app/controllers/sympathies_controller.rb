077a411639a60a90eac592a7996184ef08b08dc7
dali <dalikim@parti.xyz>
2017-09-22 10:37:58 +0900

    추모의 소셜 이미지를 바꿉니다

--- app/controllers/sympathies_controller.rb
+++ app/controllers/sympathies_controller.rb
@@ -1,5 +1,6 @@
 class SympathiesController < ApplicationController
   load_and_authorize_resource
+  before_action :reset_meta_tags, only: :show
 
   def index
     @sympathies = Sympathy.recent
@@ -38,5 +39,15 @@ class SympathiesController < ApplicationController
 
   def sympathy_params
     params.require(:sympathy).permit(:title, :body, :cover_image, :social_image)
+  end
+
+  def reset_meta_tags
+    prepare_meta_tags({
+      title: "[추모] " + @sympathy.title.html_safe,
+      description: @sympathy.body.html_safe,
+      url: request.original_url,
+      image: (view_context.image_url(@sympathy.social_image_url) if @sympathy.social_image_url),
+    }
+    )
   end
 end
