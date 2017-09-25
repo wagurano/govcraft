71522d3dc11201010efec374c073fa0a0240a94f
ganguri <ganguri@parti.xyz>
2017-09-25 10:12:29 +0900

    kdemo 빠띠에서 우리만나 공지사항을 가져옵니다

--- app/jobs/parti_hightlight_posts_job.rb
+++ app/jobs/parti_hightlight_posts_job.rb
@@ -3,7 +3,7 @@ class PartiHightlightPostsJob
   sidekiq_options unique: :while_executing
 
   def perform
-    highlight_posts = RestClient.get "#{ENV["PARTI_API_BASE"]}v1/groups/union/highlight_posts?limit=10"
+    highlight_posts = RestClient.get "#{ENV["PARTI_API_BASE"]}v1/groups/kdemo/highlight_posts?limit=10"
     Rails.cache.write "urimanna_parti_highlight_posts", JSON.parse(highlight_posts.body)
   end
 end
