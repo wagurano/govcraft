7c9c3d638cedb1ce2737481e9d0b21ddd383122d
dali <dalikim@parti.xyz>
2017-09-28 14:39:04 +0900

    아카이브 카테고리를 지울 때 오류를 바로 잡습니다

--- app/models/archive_category.rb
+++ app/models/archive_category.rb
@@ -2,7 +2,7 @@ class ArchiveCategory < ApplicationRecord
   belongs_to :archive
   belongs_to :parent, class_name: ArchiveCategory, optional: true
   has_many :children, class_name: ArchiveCategory, dependent: :destroy, foreign_key: :parent_id
-  has_many :documents, class_name: ArchiveDocument, dependent: :nullify
+  has_many :documents, class_name: ArchiveDocument, dependent: :nullify, primary_key: :slug, foreign_key: :category_slug
 
   accepts_nested_attributes_for :children, allow_destroy: true
 
