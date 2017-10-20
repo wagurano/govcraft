da7acb23a9b59b22831a09227b3913236f473efb
dali <dalikim@parti.xyz>
2017-10-20 12:06:11 +0900

    아카이브로 실험을 시작합니다

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,9 @@
 class Organization < ApplicationRecord
   has_many :projects, dependent: :nullify
   has_many :project_categories, dependent: :nullify
+
+  def subdomain
+    [slug, ApplicationController.helpers.root_subdomain].compact.join(".")
+  end
 end
+
