c4959bfeac3bfe7cef716835343f1cd546a1101b
dali <dalikim@parti.xyz>
2017-11-23 08:41:31 +0900

    스피커 목록 페이지를 만듭니다

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -1,6 +1,9 @@
 class SpeakersController < ApplicationController
   load_and_authorize_resource
 
+  def index
+  end
+
   def show
     @agendas = Agenda.where(id: Issue.where(id: @speaker.opinions.select(:issue_id)).select(:agenda_id).distinct)
     if params[:agenda_id].present?
