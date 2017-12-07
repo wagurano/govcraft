3f304c365ae528cf666d49ae87c729755b2b21f4
ganguri <ganguri@parti.xyz>
2017-12-07 17:30:12 +0900

    조직마다 다른 스타일 지정이 가능합니다

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( emails/* )
+Rails.application.config.assets.precompile << %r{\Aorganizations/(?!_)([^\/]*)/(?!_)([^\/]*)\.(css|sass|scss)\z}x
