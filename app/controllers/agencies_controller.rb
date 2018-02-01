f332d94dbef68729861d2daf4086fbe42dbf653f
rest515 <rest515@gmail.com>
2018-02-01 14:20:21 +0900

    대리기관 상세 페이지를 간단히 만듭니다. 이후에 업그레이드합니다

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -1,6 +1,7 @@
 class AgenciesController < ApplicationController
+  load_and_authorize_resource find_by: :slug
+
   def show
-    @agency = Agency.find_by(slug: 'blue_house')
     @speakers = Speaker.tagged_with(@agency.position_list, on: :positions, any: true).order(:name)
     @petitions = Petition.where(id: PetitionsSpeakers.where(speaker: @speakers).select(:petition_id))
   end
