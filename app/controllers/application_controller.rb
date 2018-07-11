5b822474cbfef8e12b0de30adecfbc140218f107
Greenhair <martionlee@gmail.com>
2018-07-11 13:32:18 +0900

    소소한 오타를 바로잡습니다

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -65,7 +65,7 @@ class ApplicationController < ActionController::Base
   def build_meta_options(options)
     current_organization = fetch_organization_from_request
     site_name = options[:site_name] || current_organization.try(:title) || "가브크래프트"
-    title = view_context.strip_tags(options[:title]) || current_organization.try(:title) || "세상을 바꾸는 시민들의 알상 정치 플랫폼, 가브크래프트"
+    title = view_context.strip_tags(options[:title]) || current_organization.try(:title) || "세상을 바꾸는 시민들의 일상 정치 플랫폼, 가브크래프트"
     image = options[:image] || (current_organization.try(:seo_image_path) and view_context.image_url(current_organization.try(:seo_image_path))) || view_context.image_url('seo.png')
     url = options[:url] || root_url
     description = view_context.strip_tags(options[:description]) || "세상을 바꾸는 시민들의 일상 정치 플랫폼 '가브크래프트'입니다"
