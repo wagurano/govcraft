4b0868289fa5c91ec4616ef8be70e237988eac64
dali <dalikim@parti.xyz>
2018-08-31 02:15:24 +0900

    기술적인 수정 - 마이그레이션을 위해 이벤트의 이미지 주소를 바로 잡습니다

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -16,7 +16,8 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    model_folder_name = model.class.to_s.underscore.gsub /^deprecated_/, ''
+    "uploads/#{model_folder_name}/#{mounted_as}/#{model.id}"
   end
 
   def content_type_whitelist
