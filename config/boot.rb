e8774731e1dc86ca1b67fd7ac86d3b5e77600c1b
wagurano <wagurano@gmail.com>
2018-09-29 11:59:02 +0900

    기술적인 수정 - 환경변수 로딩할 때 오타 수정

--- config/boot.rb
+++ config/boot.rb
@@ -1,8 +1,8 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require ‘dotenv’
-env_file_path = “config/env.custom”
+require 'dotenv'
+env_file_path = "config/env.custom"
 if File.exists?(env_file_path)
  Dotenv.load(env_file_path)
 end
